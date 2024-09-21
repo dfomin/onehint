679fa00f5c949d289eca4515067d2922445e5a9e
Dmitry Fomin <fomin.dm@gmail.com>
2024-09-21 12:09:34 +0200

    Update statistics to new API

--- onehint/statistics/info.py
+++ onehint/statistics/info.py
@@ -1,8 +1,8 @@
 import json
 from collections import defaultdict
 
-from onehint import main
-from onehint.main import versions_info, RoundWords
+from onehint import checkers
+from onehint.main import versions_info
 
 
 def analyze_versions():
@@ -13,12 +13,13 @@ def analyze_versions():
         record_results = []
         for version_info in versions_info():
             version = version_info.version
-            f = getattr(main, f"find_duplicates_v{version}")
-            response = f(RoundWords(words=list(record.keys())))
+            module = getattr(checkers, f"v{version}")
+            api_version = getattr(module, f"APIv{version}")
+            response = api_version().find_duplicates(list(record.keys()))
             expected_response = list(record.values())
-            if response.words == expected_response:
+            if response == expected_response:
                 stats[version] += 1
-            record_results.append(response.words == expected_response)
+            record_results.append(response == expected_response)
         if not all(x == record_results[0] for x in record_results):
             print(record, record_results)
     for key in stats:
